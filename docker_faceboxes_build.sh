92447a1943ce8b67412093cf2aebecfc092c3e93
caiyueliang <393900414@qq.com>
2019-03-31 16:18:51 +0800

    update

--- docker_faceboxes_build.sh
+++ docker_faceboxes_build.sh
@@ -1,6 +1,6 @@
 # HOST_IP=registry-face.maysatech.com
 #build
-docker build -t faceboxes_depend:v1.0 -f Dockerfile .
+docker build -t faceboxes_depend:v1.0 -f Dockerfile_faceboxes .
 
 # tag and push
 # docker tag faceboxes_depend:v1.0 $HOST_IP:5000/faceboxes_depend:v1.0
